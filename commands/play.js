17c0803617a06a68a00f78bf6e6361165a2f0239
AgusPerti <aperticaro@fi.uba.ar>
2021-06-19 15:05:44 -0300

    Changed play function

--- commands/play.js
+++ commands/play.js
@@ -125,7 +125,22 @@ module.exports = {
           return;
         }
 
-        const dispatcher = serverQueue.connection
+        const stream = ytdl(song.url, {filter: 'audioonly'});
+        serverQueue.connection.play(stream, {seek: 0, volume: 1})
+        .on('finish', () => {
+          if (serverQueue.loop === true) {
+            play(guild, serverQueue.songs[0]);
+            serverQueue.loop = false;
+          } else {
+            serverQueue.songs.shift();
+            play(guild, serverQueue.songs[0]);
+          }
+        });
+
+        embedSong(false, false, serverQueue.songs[0]);
+      }
+
+        /*const dispatcher = serverQueue.connection
           .play(ytdl(song.url))
           .on('finish', () => {
             if (serverQueue.loop === true) {
@@ -138,6 +153,6 @@ module.exports = {
           })
   
           embedSong(false, false, serverQueue.songs[0]);
-      }
+      }*/
     } 
 }
